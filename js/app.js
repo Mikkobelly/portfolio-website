f4be4a77cccfa8046aa07d06076446ce82169b5a
Mikkobelly <mikiakuta0529@gmail.com>
2023-01-25 04:53:17 +0000

    Updated collapsible nav functionality and added js form validation

--- js/app.js
+++ js/app.js
@@ -1,25 +1,112 @@
-const menuIcon = document.querySelector('.dropdown__menu');
-const closeIcon = document.querySelector('.dropdown__close');
-const menuIconBox = document.querySelector('.dropdown__menu-box');
-const closeIconBox = document.querySelector('.dropdown__close-box');
+//Collapsible navigation
+(function (navigation) {
+    const menuIcon = document.querySelector('.dropdown__menu');
+    const closeIcon = document.querySelector('.dropdown__close');
+    const menuIconBox = document.querySelector('.dropdown__menu-box');
+    const closeIconBox = document.querySelector('.dropdown__close-box');
 
-const navList = document.querySelector('.navigation-list');
-const navItems = document.querySelector('.navigation-list__items');
-
-
-menuIcon.addEventListener('click', () => {
-    navItems.style.transition = 'all .4s linear .3s';
-    navList.style.width = '45%';
-    navItems.style.opacity = '1';
-    menuIconBox.style.display = 'none';
-    closeIconBox.style.visibility = 'visible';
-})
+    const navList = document.querySelector('.navigation-list');
+    const navItems = document.querySelector('.navigation-list__items');
 
 
-closeIcon.addEventListener('click', () => {
-    navItems.style.transition = 'all .1s linear';
-    navList.style.width = '0%';
-    navItems.style.opacity = '0';
-    menuIconBox.style.display = 'inline-block';
-    closeIconBox.style.visibility = 'hidden';
-})
+    menuIcon.addEventListener('click', () => {
+        navList.classList.add('visible');
+        menuIconBox.classList.add('hidden');
+    })
+
+
+    closeIcon.addEventListener('click', () => {
+        navList.classList.remove('visible');
+        menuIconBox.classList.remove('hidden');
+    })
+})();
+
+
+//Contact form validation
+(function form() {
+    const form = document.querySelector('.contact__form');
+    const nameInput = document.querySelector('input[name="name"]');
+    const emailInput = document.querySelector('input[name="email"]');
+    const telInput = document.querySelector('input[name="tel"]');
+
+
+    function showErrMsg(input, message) {
+        let container = input.parentElement;
+
+        //Remove an exisiting error
+        let error = container.querySelector('.error-message');
+        if (error) {
+            container.removeChild(error);
+        }
+
+        //Add the error if the message is not empty
+        if (message) {
+            let error = document.createElement('p');
+            error.classList.add('error-message');
+            error.innerText = message;
+            container.appendChild(error);
+        }
+    };
+
+
+    function validateNameInput() {
+        if (!nameInput.value) {
+            showErrMsg(nameInput, 'Name is a required filed');
+            return false;
+        }
+
+        showErrMsg(nameInput, null);
+        return true;
+    };
+
+
+    function validateEmailInput() {
+        if (!emailInput.value) {
+            showErrMsg(emailInput, 'Email is a required filed');
+            return false;
+        } else if (emailInput.value.indexOf('@') === -1
+            || emailInput.value.indexOf('.') === -1) {
+            showErrMsg(emailInput, 'Enter a valid email address');
+            return false;
+        }
+
+        showErrMsg(emailInput, null);
+        return true;
+    };
+
+
+    function validateTelInput() {
+        const parsedInput = Number(telInput.value);
+        if (isNaN(parsedInput)) {
+            showErrMsg(telInput, 'Enter a valid telephone number');
+            return false;
+        }
+
+        showErrMsg(telInput, null);
+        return true;
+    };
+
+
+    function validateForm() {
+        const isValidNameInput = validateNameInput();
+        const isValidEmailInput = validateEmailInput();
+        const isValidTelInput = validateTelInput();
+
+        return isValidNameInput && isValidEmailInput && isValidTelInput;
+    };
+
+
+    nameInput.addEventListener('input', validateNameInput);
+    emailInput.addEventListener('input', validateEmailInput);
+    telInput.addEventListener('input', validateTelInput);
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        if (validateForm()) {
+            alert('Form submitted!');
+        } else {
+            alert('Please make adjustment to fullfill requirments')
+        }
+    });
+
+})();
